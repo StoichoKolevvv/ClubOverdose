316b69dab15142c5bde8487e1934fa20e1f22f02
Radoinov <radoinov08@gmail.com>
2026-03-04 12:12:49 +0200

    AddedControllers

---
+++ ClubOverdose/Controllers/DrinksController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ClubOverdose.Data;
+
+namespace ClubOverdose.Controllers
+{
+    public class DrinksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DrinksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Drinks
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Drinks.Include(d => d.Menu).Include(d => d.Types);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Drinks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var drink = await _context.Drinks
+                .Include(d => d.Menu)
+                .Include(d => d.Types)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (drink == null)
+            {
+                return NotFound();
+            }
+
+            return View(drink);
+        }
+
+        // GET: Drinks/Create
+        public IActionResult Create()
+        {
+            ViewData["MenuId"] = new SelectList(_context.Menus, "Id", "Id");
+            ViewData["TypeId"] = new SelectList(_context.Types, "Id", "Id");
+            return View();
+        }
+
+        // POST: Drinks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,TypeId,MenuId,Price,Volume,DateAdded")] Drink drink)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(drink);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MenuId"] = new SelectList(_context.Menus, "Id", "Id", drink.MenuId);
+            ViewData["TypeId"] = new SelectList(_context.Types, "Id", "Id", drink.TypeId);
+            return View(drink);
+        }
+
+        // GET: Drinks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var drink = await _context.Drinks.FindAsync(id);
+            if (drink == null)
+            {
+                return NotFound();
+            }
+            ViewData["MenuId"] = new SelectList(_context.Menus, "Id", "Id", drink.MenuId);
+            ViewData["TypeId"] = new SelectList(_context.Types, "Id", "Id", drink.TypeId);
+            return View(drink);
+        }
+
+        // POST: Drinks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,TypeId,MenuId,Price,Volume,DateAdded")] Drink drink)
+        {
+            if (id != drink.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(drink);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DrinkExists(drink.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MenuId"] = new SelectList(_context.Menus, "Id", "Id", drink.MenuId);
+            ViewData["TypeId"] = new SelectList(_context.Types, "Id", "Id", drink.TypeId);
+            return View(drink);
+        }
+
+        // GET: Drinks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var drink = await _context.Drinks
+                .Include(d => d.Menu)
+                .Include(d => d.Types)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (drink == null)
+            {
+                return NotFound();
+            }
+
+            return View(drink);
+        }
+
+        // POST: Drinks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var drink = await _context.Drinks.FindAsync(id);
+            if (drink != null)
+            {
+                _context.Drinks.Remove(drink);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DrinkExists(int id)
+        {
+            return _context.Drinks.Any(e => e.Id == id);
+        }
+    }
+}
